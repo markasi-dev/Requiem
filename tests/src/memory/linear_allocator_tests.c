a8444ee1f3d2e609a18157a4ecadf3f856420d81
markasi-dev <markasiv123@gmail.com>
2026-05-28 20:11:51 +0300

    Linear allocator tests complete and some fixies

--- tests/src/memory/linear_allocator_tests.c
+++ tests/src/memory/linear_allocator_tests.c
@@ -24,6 +24,94 @@ u8 linear_allocator_should_create_and_destroy() {
     return TRUE;
 }
 
+u8 linear_allocation_single_allocation_all_space() {
+    linear_allocator alloc;
+    linear_allocator_create(sizeof(u64), 0, &alloc);
+
+    // Single allocation
+    void* block = linear_allocator_allocate(&alloc, sizeof(u64));
+    
+    // Validate it
+    expect_should_not_be(0, block);
+    expect_should_be(sizeof(u64), alloc.allocated);
+
+    linear_allocator_destroy(&alloc);
+
+    return TRUE;
+}
+
+u8 linear_allocation_multi_allocation_all_space() {
+    u64 max_allocs = 1024;
+    linear_allocator alloc;
+    linear_allocator_create(sizeof(u64) * max_allocs, 0, &alloc);
+
+    // Multiple allocations - full.
+    void* block;
+    for (u64 i = 0; i < max_allocs; ++i) {
+        block = linear_allocator_allocate(&alloc, sizeof(u64));
+        // Validate if
+        expect_should_not_be(0, block);
+        expect_should_be(sizeof(u64) * (i + 1), alloc.allocated);
+    }
+
+    linear_allocator_destroy(&alloc);
+
+    return TRUE;
+}
+
+u8 linear_allocation_multi_allocation_over_allocate() {
+    u64 max_allocs = 1024;
+    linear_allocator alloc;
+    linear_allocator_create(sizeof(u64) * max_allocs, 0, &alloc);
+
+    // Multiple allocations - full.
+    void* block;
+    for (u64 i = 0; i < max_allocs; ++i) {
+        block = linear_allocator_allocate(&alloc, sizeof(u64));
+        // Validate if
+        expect_should_not_be(0, block);
+        expect_should_be(sizeof(u64) * (i + 1), alloc.allocated);
+    }
+
+    RQ_DEBUG("HEADS UP: The following error is intentionally caused by this test.");
+
+    // Ask for one more allocation. Should error and return 0.
+    block = linear_allocator_allocate(&alloc, sizeof(u64));
+    // Validate it - allocated should be unchanged.
+    expect_should_be(0, block);
+    expect_should_be(sizeof(u64) * (max_allocs), alloc.allocated);
+
+    linear_allocator_destroy(&alloc);
+
+    return TRUE;
+}
+
+u8 linear_allocator_multi_allocation_all_space_then_free() {
+    u64 max_allocs = 1024;
+    linear_allocator alloc;
+    linear_allocator_create(sizeof(u64) * max_allocs, 0, &alloc);
+
+    // Multiple allocations - full.
+    void* block;
+    for (u64 i = 0; i < max_allocs; ++i) {
+        block = linear_allocator_allocate(&alloc, sizeof(u64));
+        // Validate if
+        expect_should_not_be(0, block);
+        expect_should_be(sizeof(u64) * (i + 1), alloc.allocated);
+    }
+
+    linear_allocator_free_all(&alloc);
+    expect_should_be(0, alloc.allocated);
+
+    linear_allocator_destroy(&alloc);
+
+    return TRUE;
+}
+
 void linear_allocator_register_tests() {
     test_manager_register_test(linear_allocator_should_create_and_destroy, "Linear allocator should create and destroy");
+    test_manager_register_test(linear_allocation_single_allocation_all_space, "Linear allocator single alloc for all space");
+    test_manager_register_test(linear_allocation_multi_allocation_all_space, "Linear allocator multi alloc for all space");
+    test_manager_register_test(linear_allocation_multi_allocation_over_allocate, "Linear allocator try over allocate");
+    test_manager_register_test(linear_allocator_multi_allocation_all_space_then_free, "Linear allocator allocated should be 0 after free_all");
 }   
